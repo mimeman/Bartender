afdaaa777427c642a6df1707bd006cc5d45d2247
Fachidiot <54926396+Fachidiot@users.noreply.github.com>
2025-05-15 13:11:44 +0900

    # Tools imported
    vHirearachy
    vInspector
    vFolder

---
+++ Bartender/Assets/3. Scripts/DisableGrabbingHandModel.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class DisableGrabbingHandModel : MonoBehaviour
+{
+    // Grab 이벤트시 손 가리는게 필요한 모델에 적용.
+    public GameObject m_leftHandModel;
+    public GameObject m_rightHandModel;
+
+    void Start()
+    {
+        XRGrabInteractable grabInteractable = GetComponent<XRGrabInteractable>();
+        grabInteractable.selectEntered.AddListener(HideGrabbingHand);
+        grabInteractable.selectExited.AddListener(ShowGrabbingHand);
+    }
+
+    public void HideGrabbingHand(SelectEnterEventArgs args)
+    {
+        if (args.interactorObject.transform.tag == "Left Hand")
+            m_leftHandModel.SetActive(false);
+        else if (args.interactorObject.transform.tag == "Right Hand")
+            m_rightHandModel.SetActive(false);
+    }
+
+    public void ShowGrabbingHand(SelectExitEventArgs args)
+    {
+        if (args.interactorObject.transform.tag == "Left Hand")
+            m_leftHandModel.SetActive(true);
+        else if (args.interactorObject.transform.tag == "Right Hand")
+            m_rightHandModel.SetActive(true);
+    }
+}
